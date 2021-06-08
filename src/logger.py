5a5eed5aa1710378091c0c527fdb941d06a71f50
virgilsisoe <sisoe24@gmail.com>
2021-06-08 12:46:10 -0400

    create log if doesnt exists

--- src/logger.py
+++ src/logger.py
@@ -12,18 +12,21 @@ LOGGER = logging.getLogger('NodeBox')
 LOGGER.setLevel(logging.DEBUG)
 LOGGER.setLevel(logging.INFO)
 
-LOG_PATH = os.path.dirname(os.path.dirname(__file__))
+LOG_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'log')
+
+if not os.path.exists(LOG_PATH):
+    os.mkdir(LOG_PATH)
 
 CRITICAL_FORMAT = logging.Formatter(
     '%(asctime)s - [%(levelname)s] - %(module)s:%(lineno)d:%(funcName)s() - %(message)s')
-CRITICAL = logging.FileHandler(os.path.join(LOG_PATH, 'log/errors.log'), 'a')
+CRITICAL = logging.FileHandler(os.path.join(LOG_PATH, 'errors.log'), 'a')
 CRITICAL.setLevel(logging.WARNING)
 CRITICAL.setFormatter(CRITICAL_FORMAT)
 LOGGER.addHandler(CRITICAL)
 
 BASE_FORMAT = logging.Formatter(
     '%(asctime)s - %(filename)-20s %(funcName)-25s %(levelname)-10s %(message)s')
-DEBUG = logging.FileHandler(os.path.join(LOG_PATH, 'log/debug.log'), 'w')
+DEBUG = logging.FileHandler(os.path.join(LOG_PATH, 'debug.log'), 'w')
 DEBUG.setLevel(logging.DEBUG)
 DEBUG.setFormatter(BASE_FORMAT)
 LOGGER.addHandler(DEBUG)
