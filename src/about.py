defb9fd4ca4399ae243855469950e6f0211efa45
virgilsisoe <sisoe24@gmail.com>
2021-08-04 23:46:25 -0400

    about first commit

---
+++ src/about.py
@@ -0,0 +1,113 @@
+# coding: utf-8
+from __future__ import print_function
+
+from os.path import (
+    basename, join, exists, dirname, abspath
+)
+
+from subprocess import check_output
+from collections import OrderedDict
+
+from PySide2 import __version__ as PySide2_Version
+from PySide2.QtCore import __version__ as QtCore_version
+from PySide2.QtCore import QSysInfo
+
+
+def get_git_branch():
+    """Get git branch name if any."""
+    branch = ""
+    try:
+        # git > 2.22 could do 'git branch --show-current'
+        branch = check_output(
+            ['git', 'rev-parse', '--abbrev-ref', 'HEAD'])
+
+    # No git installed or project downloaded as a .zip
+    except Exception:
+        pass
+
+    return branch.decode('utf-8').strip()
+
+
+def _get_root():
+    """Get root (package) absolute path."""
+    path = dirname(dirname(abspath(__file__)))
+    return path
+
+
+def _get_package_name():
+    """Get package name."""
+    return basename(_get_root())
+
+
+def _get_package_version():
+    """Get package version. If no file return empty"""
+    file = join(_get_root(), 'VERSION')
+
+    if exists(file):
+        with open(file) as file:
+            return file.read()
+
+    return ''
+
+
+PACKAGE = _get_package_name()
+
+
+def _clean_empty(_list):
+    """Don't include key if value is empty"""
+    return [(k, v) for k, v in _list if v]
+
+
+def about():
+    _about = (
+        (PACKAGE, _get_package_version()),
+        ('PySide2', PySide2_Version),
+        ('PySide2.QtCore', QtCore_version),
+        ('Machine', QSysInfo().prettyProductName()),
+        ('Build', get_git_branch()),
+        ('Other', '')
+    )
+
+    return _clean_empty(_about)
+
+
+def about_links():
+    github_web = 'https://github.com/sisoe24/' + PACKAGE
+
+    _about_links = (
+        ('Github', github_web),
+        ('Readme', github_web + '/blob/main/README.md'),
+        ('Changelog', github_web + '/blob/main/CHANGELOG.md'),
+        ('Issues', github_web + '/issues'),
+        ('Nukepedia', 'https://www.nukepedia.com/python/ui/profileinspector'),
+        ('Other', '')
+    )
+
+    return _clean_empty(_about_links)
+
+
+def get_about_key(key):
+    """Get a specific key in about. return empty string if not found."""
+    for abouts in [about(), about_links()]:
+        for item in abouts:
+            if key in item:
+                return item[1]
+    return ''
+
+
+def about_to_string(exclude=None):
+    """Get about dict and convert it into a multi line string.
+
+    Args:
+        exclude (list | str): list of str keys from the about dict to exclude in the return.
+    """
+    about_str = ''
+    for key, value in about():
+
+        if exclude:
+            if key in exclude:
+                continue
+
+        about_str += '%s: %s\n' % (key, value)
+
+    return about_str
